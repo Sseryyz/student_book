064635f62ecd9d78a82f71050aa181094045f1ad
Sseryyz <12serzhj@gmail.com>
2019-10-31 17:19:33 +0200

    added 3 students

---
+++ main.js
@@ -0,0 +1,52 @@
+
+let gradebook = {
+    math: "",
+    physics: "",
+}
+
+function Student() {
+    this.name = name,
+    this.gradebook = gradebook;
+}
+
+let Vasya = new Student([
+    this.name = "Vasya",
+    gradebook = {
+    math: 5,
+    physics: 4,
+    }
+]);
+
+let Petr = new Student([
+    this.name = "Petr",
+    gradebook = {
+        math: 3,
+        physics: 5,
+    }
+]);
+
+let Ivan = new Student([
+    this.name = "Ivan",
+    gradebook = {
+        math: 4,
+        physics: 5,
+    }
+]);
+
+console.log(Vasya, Petr, Ivan);
+
+let group = {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
