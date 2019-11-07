f1f45b53e21b87839e2ed0731b0aeac342e47209
Sseryyz <12serzhj@gmail.com>
2019-11-07 18:16:05 +0200

    refactored

--- starwars.js
+++ starwars.js
@@ -1,24 +1,14 @@
 (async () =>{
     const {results: people } = await fetch('https://swapi.co/api/people/').then((res) => res.json());
 
-    persons = {results: people};
+    let persons = {results: people};
 
-    result = {};
     for (let i = 0; i < persons.results.length; i++) {
-        result = ["Name:" + persons.results[i].name,
-                        "Mass: " + persons.results[i].mass,
-                        "Height: " + persons.results[i].height,
-                        "Volume: " + +persons.results[i].mass * +persons.results[i].height
-                        ];
+        let result = [
+             "Name:" + persons.results[i].name,
+             "Mass: " + persons.results[i].mass,
+             "Height: " + persons.results[i].height,
+             "Volume: " + +persons.results[i].mass * +persons.results[i].height];
         console.log(result);
     }
-    // const result = {
-    //     "Luck SkyWalker": {
-    //         mass: 123213,
-    //         height: 123123,
-    //         volume: this.mass * this.height,
-    //     }
-    // };
 })();
-let persons;
-let result;
