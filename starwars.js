c4a440aeb5e09772129fa8d63590bbe67c769143
Sseryyz <12serzhj@gmail.com>
2019-11-08 17:38:54 +0200

    res changed to obj

--- starwars.js
+++ starwars.js
@@ -2,13 +2,19 @@
     const {results: people } = await fetch('https://swapi.co/api/people/').then((res) => res.json());
 
     let persons = {results: people};
+    const result = {};
 
     for (let i = 0; i < persons.results.length; i++) {
-        let result = [
-             "Name:" + persons.results[i].name,
-             "Mass: " + persons.results[i].mass,
-             "Height: " + persons.results[i].height,
-             "Volume: " + +persons.results[i].mass * +persons.results[i].height];
+        result[i] = {
+             Name: persons.results[i].name,
+             Mass: persons.results[i].mass,
+             Height: persons.results[i].height,
+             Volume: +persons.results[i].mass * +persons.results[i].height,};
         console.log(result);
     }
 })();
+
+
+
+
+
